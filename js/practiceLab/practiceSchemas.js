5c160b764b2e2d1219ec223dcffbabb7c1cad6d5
thiepn <junsosmart@gmail.com>
2026-09-08 12:59:49 +0200

    PL25: register coachPlan record type

--- js/practiceLab/practiceSchemas.js
+++ js/practiceLab/practiceSchemas.js
@@ -26,6 +26,7 @@ export const PRACTICE_RECORD_TYPES = Object.freeze({
   learningState: Object.freeze({ storeName: "learningStates", versionField: "recordVersion" }),
   evaluationState: Object.freeze({ storeName: "evaluationStates", versionField: "recordVersion" }),
   assessmentRun: Object.freeze({ storeName: "assessmentRuns", versionField: "recordVersion" }),
+  coachPlan: Object.freeze({ storeName: "coachPlans", versionField: "recordVersion" }),
   sessionSummary: Object.freeze({ storeName: "sessionSummaries", versionField: "recordVersion" }),
   reviewItem: Object.freeze({ storeName: "reviewItems", versionField: "recordVersion" }),
   customText: Object.freeze({ storeName: "customTexts", versionField: "recordVersion" }),
