8798d99044902e3e2ab881b4a0764b986be01d1e
thiepn <junsosmart@gmail.com>
2026-09-11 14:41:15 +0200

    V8: centralize presentation boot modules

---
+++ js/presentationBootstrap.js
@@ -0,0 +1,8 @@
+// Semantic owner for DOM presentation enhancers that sit outside main.js.
+// Keep presentation boot order explicit and out of index.html.
+import "./campaignGameplayPresentation.js";
+import "./endlessGameplayPresentation.js";
+import "./bossGameplayPresentation.js";
+import "./arcadeRushGameplayPresentation.js";
+import "./profileLeaderboardsSettingsPresentation.js";
+import "./ui12GlobalPresentation.js";
