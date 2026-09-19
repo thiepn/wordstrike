903c2b081f177a0c9597a9142b4820aff14a0a63
thiepn <junsosmart@gmail.com>
2026-09-19 20:56:04 +0200

    fix(practice): refresh offline cache for DB13 repair

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v17";
+const CACHE_NAME = CACHE_PREFIX + "v18";
 const APP_SHELL = [
   "./js/practiceLab/practiceLabWorkshop.js",
   "./practiceLabWorkshop.css",
