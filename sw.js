6c15e559469b49f46620f92edf171517e96a5990
thiepn <junsosmart@gmail.com>
2026-09-20 02:00:41 +0200

    fix(practice): refresh offline cache for stable Research input

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v25-boss-input";
+const CACHE_NAME = CACHE_PREFIX + "v26-practice-inputs";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
