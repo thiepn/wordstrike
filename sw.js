8d2b372929dacd060ed081bd0d65c8fb1a58a6b4
thiepn <junsosmart@gmail.com>
2026-09-20 15:44:50 +0200

    fix(practice): refresh offline cache for atomic reset

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v33-practice-storage-access";
+const CACHE_NAME = CACHE_PREFIX + "v34-practice-reset";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
