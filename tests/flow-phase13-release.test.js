e3711d0284d63ccc0f4a7825b04403a04cb00024
thiepn <junsosmart@gmail.com>
2026-09-23 18:49:40 +0200

    Update Flow release contracts for Corpus V2

--- tests/flow-phase13-release.test.js
+++ tests/flow-phase13-release.test.js
@@ -96,17 +96,19 @@ for (const key of FLOW_RELEASE_QUERY_KEYS) {
 assert.ok(FLOW_RELEASE_ASSETS.length >= 30, "release cache pack should cover the complete Flow stack");
 assert.equal(new Set(FLOW_RELEASE_ASSETS).size, FLOW_RELEASE_ASSETS.length, "release cache pack contains duplicates");
 for (const asset of [
-  "./js/flow/flowRuntimeLoader.js?v=20260923d",
+  "./js/flow/flowRuntimeLoader.js?v=20260923e",
   "./js/flow/flowEngine.js",
   "./js/flow/flowCadence.js",
   "./js/flow/flowGameplay.js",
   "./js/flow/flowGameModeV2.js?v=20260923c",
   "./js/flow/flowContentExpansion.js",
+  "./js/flow/flowCorpusV2.js?v=20260923a",
+  "./js/flow/flowCorpusHistory.js?v=20260923a",
   "./js/flow/flowLongformContent.js",
   "./js/flow/flowPassages.js",
-  "./js/flow/flowPhase1.js?v=20260923d",
+  "./js/flow/flowPhase1.js?v=20260923e",
   "./js/flow/flowProgression.js",
-  "./js/flow/flowRunPlan.js?v=20260923a",
+  "./js/flow/flowRunPlan.js?v=20260923e",
   "./js/flow/flowScoreV2.js?v=20260923a",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowProgression.js?v=20260923a",
@@ -120,7 +122,7 @@ for (const asset of [
 }
 
 const mainIndex = index.indexOf('src="js/main.js?v=20260923d"');
-const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260923d"');
+const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260923e"');
 assert.ok(mainIndex >= 0 && releaseIndex > mainIndex, "main.js must boot before the release loader can temporarily emulate the developer route");
 assert.doesNotMatch(index, /src="js\/flow\/flowPhase1\.js/);
 assert.doesNotMatch(index, /const flowParams = new URLSearchParams/);
