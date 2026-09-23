d4d12dc5d8b8a2f38bea5610f392afe56466d0a4
thiepn <junsosmart@gmail.com>
2026-09-23 16:15:34 +0200

    test(release): track refreshed main bundle

--- tests/flow-phase13-release.test.js
+++ tests/flow-phase13-release.test.js
@@ -115,7 +115,7 @@ for (const asset of [
   assert.ok(FLOW_RELEASE_ASSETS.includes(asset), `offline pack missing ${asset}`);
 }
 
-const mainIndex = index.indexOf('src="js/main.js?v=20260910f"');
+const mainIndex = index.indexOf('src="js/main.js?v=20260923b"');
 const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260923b"');
 assert.ok(mainIndex >= 0 && releaseIndex > mainIndex, "main.js must boot before the release loader can temporarily emulate the developer route");
 assert.doesNotMatch(index, /src="js\/flow\/flowPhase1\.js/);
