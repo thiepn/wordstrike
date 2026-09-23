e70fd789cf3493d4867ac14f4e0f5af924a20a5f
thiepn <junsosmart@gmail.com>
2026-09-23 17:47:44 +0200

    test(flow-v2): align hardened release asset versions

--- tests/flow-phase13-release.test.js
+++ tests/flow-phase13-release.test.js
@@ -96,22 +96,22 @@ for (const key of FLOW_RELEASE_QUERY_KEYS) {
 assert.ok(FLOW_RELEASE_ASSETS.length >= 30, "release cache pack should cover the complete Flow stack");
 assert.equal(new Set(FLOW_RELEASE_ASSETS).size, FLOW_RELEASE_ASSETS.length, "release cache pack contains duplicates");
 for (const asset of [
-  "./js/flow/flowRuntimeLoader.js?v=20260923d",
+  "./js/flow/flowRuntimeLoader.js?v=20260923e",
   "./js/flow/flowEngine.js",
   "./js/flow/flowCadence.js",
   "./js/flow/flowGameplay.js",
-  "./js/flow/flowGameModeV2.js?v=20260923c",
+  "./js/flow/flowGameModeV2.js?v=20260923d",
   "./js/flow/flowContentExpansion.js",
   "./js/flow/flowLongformContent.js",
   "./js/flow/flowPassages.js",
-  "./js/flow/flowPhase1.js?v=20260923d",
+  "./js/flow/flowPhase1.js?v=20260923e",
   "./js/flow/flowProgression.js",
   "./js/flow/flowRunPlan.js?v=20260923a",
-  "./js/flow/flowScoreV2.js?v=20260923a",
-  "./js/flow/flowRecordsV2.js?v=20260923a",
-  "./js/flow/flowProgression.js?v=20260923a",
+  "./js/flow/flowScoreV2.js?v=20260923b",
+  "./js/flow/flowRecordsV2.js?v=20260923b",
+  "./js/flow/flowProgression.js?v=20260923b",
   "./js/flow/flowUiPhase7KeyboardGuard.js?v=20260923a",
-  "./js/flow/flowIntegrationPhase11.js?v=20260923b",
+  "./js/flow/flowIntegrationPhase11.js?v=20260923c",
   "./styles/screens/flow-phase1.css?v=20260916d",
   "./styles/screens/flow-game-mode-v2.css?v=20260923c",
   "./styles/screens/flow-integration-phase11.css?v=20260916a",
@@ -120,7 +120,7 @@ for (const asset of [
 }
 
 const mainIndex = index.indexOf('src="js/main.js?v=20260923b"');
-const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260923d"');
+const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260923e"');
 assert.ok(mainIndex >= 0 && releaseIndex > mainIndex, "main.js must boot before the release loader can temporarily emulate the developer route");
 assert.doesNotMatch(index, /src="js\/flow\/flowPhase1\.js/);
 assert.doesNotMatch(index, /const flowParams = new URLSearchParams/);
