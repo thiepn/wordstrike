bd2d481ad388ca3244e8df434ec989d9804e911d
thiepn <junsosmart@gmail.com>
2026-09-23 23:32:03 +0200

    Precache instant-play Flow V3 runtime

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v67-input-navigation-redo";
+const CACHE_NAME = CACHE_PREFIX + "v68-flow-v3-instant-play";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
@@ -30,6 +30,12 @@ const APP_SHELL = [
   "./js/flow/flowContentExpansion.js",
   "./js/flow/flowCorpusV2.js",
   "./js/flow/flowCorpusHistory.js",
+  "./js/flow/flowStreamPlanV3.js",
+  "./js/flow/flowStreamPlanV3.js?v=20260923a",
+  "./js/flow/flowScoreV3.js",
+  "./js/flow/flowScoreV3.js?v=20260923a",
+  "./js/flow/flowRecordsV3.js",
+  "./js/flow/flowRecordsV3.js?v=20260923a",
   "./js/flow/flowEngine.js",
   "./js/flow/flowGameplay.js",
   "./js/flow/flowGameModeV2.js",
@@ -45,7 +51,7 @@ const APP_SHELL = [
   "./js/flow/flowModifiersPhase9.js",
   "./js/flow/flowPassages.js",
   "./js/flow/flowPhase1.js",
-  "./js/flow/flowPhase1.js?v=20260923d",
+  "./js/flow/flowPhase1.js?v=20260923i",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowRunPlan.js",
@@ -55,7 +61,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV2.js",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowRuntimeLoader.js",
-  "./js/flow/flowRuntimeLoader.js?v=20260923h",
+  "./js/flow/flowRuntimeLoader.js?v=20260923i",
   "./js/flow/flowSelection.js",
   "./js/flow/flowShell.js",
   "./js/flow/flowState.js",
@@ -88,7 +94,7 @@ const APP_SHELL = [
   "./styles/screens/flow-phase5.css",
   "./styles/screens/flow-phase5.css?v=20260916a",
   "./styles/screens/flow-game-mode-v2.css",
-  "./styles/screens/flow-game-mode-v2.css?v=20260923c",
+  "./styles/screens/flow-game-mode-v2.css?v=20260923d",
   "./styles/screens/flow-ui-phase7-polish.css",
   "./styles/screens/flow-ui-phase7-polish.css?v=20260916b",
   "./styles/screens/flow-ui-phase7.css",
