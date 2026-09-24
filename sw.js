7a31b7c161c495d42e1b259bbaaad7321d8ac30f
thiepn <junsosmart@gmail.com>
2026-09-24 06:47:28 +0200

    Precache Flow V4 hardening build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v84-state-consistency-pass5";
+const CACHE_NAME = CACHE_PREFIX + "v85-flow-v4-hardening";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
@@ -48,7 +48,7 @@ const APP_SHELL = [
   "./js/flow/flowIntegrationPhase11.js",
   "./js/flow/flowIntegrationPhase11.js?v=20260923b",
   "./js/flow/flowIdentityV1.js",
-  "./js/flow/flowIdentityV1.js?v=20260924a",
+  "./js/flow/flowIdentityV1.js?v=20260924b",
   "./js/flow/flowLongformContent.js",
   "./js/flow/flowMigrationPresentation.js",
   "./js/flow/flowMigrationPresentation.js?v=20260923a",
@@ -56,7 +56,7 @@ const APP_SHELL = [
   "./js/flow/flowModifiersPhase9.js",
   "./js/flow/flowPassages.js",
   "./js/flow/flowPhase1.js",
-  "./js/flow/flowPhase1.js?v=20260924f",
+  "./js/flow/flowPhase1.js?v=20260924g",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -68,7 +68,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV2.js",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowRuntimeLoader.js",
-  "./js/flow/flowRuntimeLoader.js?v=20260924g",
+  "./js/flow/flowRuntimeLoader.js?v=20260924h",
   "./js/flow/flowSelection.js",
   "./js/flow/flowShell.js",
   "./js/flow/flowState.js",
