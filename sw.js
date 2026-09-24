2a6c1a9e8c1d154489314310932f7f83a5cca36a
thiepn <junsosmart@gmail.com>
2026-09-24 03:25:44 +0200

    Cache Flow 7B progression offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v76-flow-session-v4-rank";
+const CACHE_NAME = CACHE_PREFIX + "v77-flow-progression-v4";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
@@ -54,9 +54,11 @@ const APP_SHELL = [
   "./js/flow/flowModifiersPhase9.js",
   "./js/flow/flowPassages.js",
   "./js/flow/flowPhase1.js",
-  "./js/flow/flowPhase1.js?v=20260924d",
+  "./js/flow/flowPhase1.js?v=20260924e",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
+  "./js/flow/flowProgressionV4.js",
+  "./js/flow/flowProgressionV4.js?v=20260924a",
   "./js/flow/flowRunPlan.js",
   "./js/flow/flowRunPlan.js?v=20260923a",
   "./js/flow/flowScoreV2.js",
@@ -64,7 +66,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV2.js",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowRuntimeLoader.js",
-  "./js/flow/flowRuntimeLoader.js?v=20260924d",
+  "./js/flow/flowRuntimeLoader.js?v=20260924e",
   "./js/flow/flowSelection.js",
   "./js/flow/flowShell.js",
   "./js/flow/flowState.js",
@@ -99,7 +101,7 @@ const APP_SHELL = [
   "./styles/screens/flow-game-mode-v2.css",
   "./styles/screens/flow-game-mode-v2.css?v=20260923d",
   "./styles/screens/flow-session-v4.css",
-  "./styles/screens/flow-session-v4.css?v=20260924a",
+  "./styles/screens/flow-session-v4.css?v=20260924b",
   "./styles/screens/flow-ui-phase7-polish.css",
   "./styles/screens/flow-ui-phase7-polish.css?v=20260916b",
   "./styles/screens/flow-ui-phase7.css",
