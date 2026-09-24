a33e6426a20f7919c8b71d06194b8429d09a68ba
thiepn <junsosmart@gmail.com>
2026-09-24 08:18:33 +0200

    Rotate Flow runtime for Pass 7

--- js/flow/flowRuntimeLoader.js
+++ js/flow/flowRuntimeLoader.js
@@ -1,8 +1,8 @@
 import { loadPreferredFlowTheme } from "./flowIdentityV1.js?v=20260924a";
 
 const RELEASE_FLAG = "flowRelease";
-const FLOW_RELEASE_VERSION = 19;
-const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v22";
+const FLOW_RELEASE_VERSION = 20;
+const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v23";
 const FLOW_OFFLINE_CACHE_BATCH_SIZE = 16;
 const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
   "mode",
@@ -27,7 +27,7 @@ const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
 ]);
 
 const FLOW_RELEASE_ASSETS = Object.freeze([
-  "./js/flow/flowRuntimeLoader.js?v=20260924j",
+  "./js/flow/flowRuntimeLoader.js?v=20260924k",
   "./js/leaderboardReturnState.js",
   "./js/pendingResultSubmission.js",
   "./js/submissionOutbox.js",
@@ -70,7 +70,7 @@ const FLOW_RELEASE_ASSETS = Object.freeze([
   "./js/flow/flowModifiers.js",
   "./js/flow/flowModifiersPhase9.js?v=20260923a",
   "./js/flow/flowPassages.js",
-  "./js/flow/flowPhase1.js?v=20260924h",
+  "./js/flow/flowPhase1.js?v=20260924i",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -395,7 +395,7 @@ async function importFlowRuntime() {
     const integrationBootstrap = await import("./flowIntegrationBootstrap.js?v=20260923b");
     integrationBootstrap.applyFlowIntegrationDefaults?.();
     await Promise.all([
-      import("./flowPhase1.js?v=20260924h"),
+      import("./flowPhase1.js?v=20260924i"),
       import("./flowVisualPhase6.js?v=20260923a"),
     ]);
 
