3b674184017a47755412f2703dd90c76d5f8d284
thiepn <junsosmart@gmail.com>
2026-09-24 04:04:46 +0200

    Align Flow V3 certification with release v15

--- tests/flow-v2-release-certification.test.js
+++ tests/flow-v2-release-certification.test.js
@@ -37,9 +37,9 @@ assert.equal(validateLeaderboardRequest({ boardKey: LEADERBOARD_BOARDS.FLOW_QUIC
 assert.equal(EXPECTED_LEADERBOARD_RULES_VERSIONS[LEADERBOARD_BOARDS.FLOW_STANDARD], 3);
 assert.equal(getLeaderboardSelection(LEADERBOARD_BOARDS.FLOW_STANDARD).selectedCategory, LEADERBOARD_CATEGORIES.FLOW);
 
-assert.match(loader, /const FLOW_RELEASE_VERSION = 14/);
-assert.match(loader, /wordstrike-flow-release-v17/);
-assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924e/);
+assert.match(loader, /const FLOW_RELEASE_VERSION = 15/);
+assert.match(loader, /wordstrike-flow-release-v18/);
+assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924f/);
 for (const asset of [
   "./js/authService.js",
   "./js/leaderboardProfileService.js",
