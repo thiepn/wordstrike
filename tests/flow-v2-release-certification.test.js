cf32333ace2988b2102b7595690070e46f1ce5fa
thiepn <junsosmart@gmail.com>
2026-09-24 02:55:54 +0200

    Align Flow Phase 7A release contracts

--- tests/flow-v2-release-certification.test.js
+++ tests/flow-v2-release-certification.test.js
@@ -37,8 +37,8 @@ assert.equal(validateLeaderboardRequest({ boardKey: LEADERBOARD_BOARDS.FLOW_QUIC
 assert.equal(EXPECTED_LEADERBOARD_RULES_VERSIONS[LEADERBOARD_BOARDS.FLOW_STANDARD], 3);
 assert.equal(getLeaderboardSelection(LEADERBOARD_BOARDS.FLOW_STANDARD).selectedCategory, LEADERBOARD_CATEGORIES.FLOW);
 
-assert.match(loader, /const FLOW_RELEASE_VERSION = 11/);
-assert.match(loader, /wordstrike-flow-release-v14/);
+assert.match(loader, /const FLOW_RELEASE_VERSION = 12/);
+assert.match(loader, /wordstrike-flow-release-v15/);
 assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924b/);
 for (const asset of [
   "./js/authService.js",
@@ -52,7 +52,7 @@ for (const asset of [
   "./js/flow/flowScoreV3.js?v=20260923a",
   "./js/flow/flowRecordsV3.js",
   "./js/flow/flowRecordsV3.js?v=20260923a",
-  "./js/flow/flowPhase1.js?v=20260924a",
+  "./js/flow/flowPhase1.js?v=20260924c",
   "./js/flow/flowCadence.js?v=20260924a",
 ]) {
   assert.equal(loader.includes(JSON.stringify(asset)), true, "offline pack missing " + asset);
