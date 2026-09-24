f55e68323e6574009b54b4b28bf4ba303eedf910
thiepn <junsosmart@gmail.com>
2026-09-24 08:18:29 +0200

    Align final Flow theme-integrity contracts

--- tests/flow-v2-release-certification.test.js
+++ tests/flow-v2-release-certification.test.js
@@ -37,9 +37,9 @@ assert.equal(validateLeaderboardRequest({ boardKey: LEADERBOARD_BOARDS.FLOW_QUIC
 assert.equal(EXPECTED_LEADERBOARD_RULES_VERSIONS[LEADERBOARD_BOARDS.FLOW_STANDARD], 3);
 assert.equal(getLeaderboardSelection(LEADERBOARD_BOARDS.FLOW_STANDARD).selectedCategory, LEADERBOARD_CATEGORIES.FLOW);
 
-assert.match(loader, /const FLOW_RELEASE_VERSION = 19/);
-assert.match(loader, /wordstrike-flow-release-v22/);
-assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924j/);
+assert.match(loader, /const FLOW_RELEASE_VERSION = 20/);
+assert.match(loader, /wordstrike-flow-release-v23/);
+assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924k/);
 for (const asset of [
   "./js/authService.js",
   "./js/leaderboardProfileService.js",
@@ -52,9 +52,9 @@ for (const asset of [
   "./js/flow/flowScoreV3.js?v=20260923a",
   "./js/flow/flowRecordsV3.js",
   "./js/flow/flowRecordsV3.js?v=20260923a",
-  "./js/flow/flowPhase1.js?v=20260924h",
+  "./js/flow/flowPhase1.js?v=20260924i",
   "./js/flow/flowProgressionV4.js?v=20260924a",
-  "./js/flow/flowIdentityV1.js?v=20260924a",
+  "./js/flow/flowIdentityV1.js?v=20260924b",
   "./styles/screens/flow-session-v4.css?v=20260924c",
   "./styles/screens/flow-session-v4.css",
   "./js/flow/flowSessionV4.js?v=20260924a",
