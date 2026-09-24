f33a57f6e9529b8c6f35de55319e7457432c8746
thiepn <junsosmart@gmail.com>
2026-09-24 06:56:33 +0200

    Rotate Flow release assets for Pass 6

--- js/flow/flowRuntimeLoader.js
+++ js/flow/flowRuntimeLoader.js
@@ -1,8 +1,8 @@
 import { loadPreferredFlowTheme } from "./flowIdentityV1.js?v=20260924a";
 
 const RELEASE_FLAG = "flowRelease";
-const FLOW_RELEASE_VERSION = 16;
-const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v19";
+const FLOW_RELEASE_VERSION = 17;
+const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v20";
 const FLOW_OFFLINE_CACHE_BATCH_SIZE = 16;
 const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
   "mode",
@@ -27,7 +27,7 @@ const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
 ]);
 
 const FLOW_RELEASE_ASSETS = Object.freeze([
-  "./js/flow/flowRuntimeLoader.js?v=20260924g",
+  "./js/flow/flowRuntimeLoader.js?v=20260924h",
   "./js/leaderboardReturnState.js",
   "./js/pendingResultSubmission.js",
   "./js/submissionOutbox.js",
@@ -52,7 +52,7 @@ const FLOW_RELEASE_ASSETS = Object.freeze([
   "./js/flow/flowAdaptive.js",
   "./js/flow/flowAdaptivePhase10.js?v=20260923a",
   "./js/flow/flowCadence.js",
-  "./js/flow/flowCadence.js?v=20260924a",
+  "./js/flow/flowCadence.js?v=20260924b",
   "./js/flow/flowCatalog.js",
   "./js/flow/flowConfig.js",
   "./js/flow/flowContent.js",
@@ -70,7 +70,7 @@ const FLOW_RELEASE_ASSETS = Object.freeze([
   "./js/flow/flowModifiers.js",
   "./js/flow/flowModifiersPhase9.js?v=20260923a",
   "./js/flow/flowPassages.js",
-  "./js/flow/flowPhase1.js?v=20260924f",
+  "./js/flow/flowPhase1.js?v=20260924g",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -393,7 +393,7 @@ async function importFlowRuntime() {
     const integrationBootstrap = await import("./flowIntegrationBootstrap.js?v=20260923b");
     integrationBootstrap.applyFlowIntegrationDefaults?.();
     await Promise.all([
-      import("./flowPhase1.js?v=20260924f"),
+      import("./flowPhase1.js?v=20260924g"),
       import("./flowVisualPhase6.js?v=20260923a"),
     ]);
 
