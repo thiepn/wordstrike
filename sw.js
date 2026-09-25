81b1eb39526c33fe73d67896c98aa57ec93bd6d2
thiepn <junsosmart@gmail.com>
2026-09-25 18:01:51 +0200

    Flow: precache word-boundary runtime v39

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v105-flow-progression-v38";
+const CACHE_NAME = CACHE_PREFIX + "v106-flow-word-boundary-v39";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
@@ -40,7 +40,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV3.js",
   "./js/flow/flowRecordsV3.js?v=20260925a",
   "./js/flow/flowEngine.js",
-  "./js/flow/flowEngine.js?v=20260925b",
+  "./js/flow/flowEngine.js?v=20260925c",
   "./js/flow/flowGameplay.js",
   "./js/flow/flowGameModeV2.js",
   "./js/flow/flowGameModeV2.js?v=20260923c",
@@ -57,7 +57,7 @@ const APP_SHELL = [
   "./js/flow/flowModifiersPhase9.js",
   "./js/flow/flowPassages.js",
   "./js/flow/flowPhase1.js",
-  "./js/flow/flowPhase1.js?v=20260925m",
+  "./js/flow/flowPhase1.js?v=20260925n",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -69,7 +69,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV2.js",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowRuntimeLoader.js",
-  "./js/flow/flowRuntimeLoader.js?v=20260925p",
+  "./js/flow/flowRuntimeLoader.js?v=20260925q",
   "./js/flow/flowSelection.js",
   "./js/flow/flowShell.js",
   "./js/flow/flowState.js",
