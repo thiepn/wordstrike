647a396d71a7ab51c39c829aaeecf229e5058855
thiepn <junsosmart@gmail.com>
2026-09-25 06:17:14 +0200

    Update Flow layout-stable typo release expectations

--- tests/flow-v2-release-certification.test.js
+++ tests/flow-v2-release-certification.test.js
@@ -37,8 +37,8 @@ assert.equal(validateLeaderboardRequest({ boardKey: LEADERBOARD_BOARDS.FLOW_QUIC
 assert.equal(EXPECTED_LEADERBOARD_RULES_VERSIONS[LEADERBOARD_BOARDS.FLOW_STANDARD], 3);
 assert.equal(getLeaderboardSelection(LEADERBOARD_BOARDS.FLOW_STANDARD).selectedCategory, LEADERBOARD_CATEGORIES.FLOW);
 
-assert.match(loader, /const FLOW_RELEASE_VERSION = 25/);
-assert.match(loader, /wordstrike-flow-release-v28/);
+assert.match(loader, /const FLOW_RELEASE_VERSION = 26/);
+assert.match(loader, /wordstrike-flow-release-v29/);
 assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924m/);
 for (const asset of [
   "./js/authService.js",
@@ -52,7 +52,7 @@ for (const asset of [
   "./js/flow/flowScoreV3.js?v=20260925b",
   "./js/flow/flowRecordsV3.js",
   "./js/flow/flowRecordsV3.js?v=20260923a",
-  "./js/flow/flowPhase1.js?v=20260925c",
+  "./js/flow/flowPhase1.js?v=20260925d",
   "./js/flow/flowProgressionV4.js?v=20260924a",
   "./js/flow/flowIdentityV1.js?v=20260924b",
   "./styles/screens/flow-session-v4.css?v=20260924c",
