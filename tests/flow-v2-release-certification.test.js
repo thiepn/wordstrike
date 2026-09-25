abf06ed8797ef10e6646512cf07d9990f340437a
thiepn <junsosmart@gmail.com>
2026-09-25 06:19:53 +0200

    Fix Flow V3 delivered-loader certification

--- tests/flow-v2-release-certification.test.js
+++ tests/flow-v2-release-certification.test.js
@@ -39,7 +39,7 @@ assert.equal(getLeaderboardSelection(LEADERBOARD_BOARDS.FLOW_STANDARD).selectedC
 
 assert.match(loader, /const FLOW_RELEASE_VERSION = 26/);
 assert.match(loader, /wordstrike-flow-release-v29/);
-assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260924m/);
+assert.match(index, /js\/flow\/flowRuntimeLoader\.js\?v=20260925d/);
 for (const asset of [
   "./js/authService.js",
   "./js/leaderboardProfileService.js",
