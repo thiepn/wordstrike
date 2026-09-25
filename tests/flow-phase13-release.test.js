c84e82395ea993b349d49285d059f996b39dbe47
thiepn <junsosmart@gmail.com>
2026-09-25 16:51:00 +0200

    Update Flow v32 offline expectations

--- tests/flow-phase13-release.test.js
+++ tests/flow-phase13-release.test.js
@@ -98,7 +98,7 @@ for (const key of FLOW_RELEASE_QUERY_KEYS) {
 assert.ok(FLOW_RELEASE_ASSETS.length >= 30, "release cache pack should cover the complete Flow stack");
 assert.equal(new Set(FLOW_RELEASE_ASSETS).size, FLOW_RELEASE_ASSETS.length, "release cache pack contains duplicates");
 for (const asset of [
-  "./js/flow/flowRuntimeLoader.js?v=20260925i",
+  "./js/flow/flowRuntimeLoader.js?v=20260925j",
   "./js/leaderboardService.js",
   "./js/supabaseConfig.js",
   "./js/supabaseClient.js",
@@ -122,7 +122,7 @@ for (const asset of [
   "./js/flow/flowPhase1.js?v=20260925h",
   "./js/flow/flowProgressionV4.js?v=20260924a",
   "./js/flow/flowIdentityV1.js?v=20260924b",
-  "./styles/screens/flow-session-v4.css?v=20260924c",
+  "./styles/screens/flow-session-v4.css?v=20260925a",
   "./styles/screens/flow-session-v4.css",
   "./js/flow/flowSessionV4.js?v=20260924a",
   "./js/flow/flowSessionV4.js",
@@ -139,7 +139,7 @@ for (const asset of [
   "./js/flow/flowUiPhase7KeyboardGuard.js?v=20260923a",
   "./js/flow/flowIntegrationPhase11.js?v=20260923b",
   "./styles/screens/flow-phase1.css?v=20260916d",
-  "./styles/screens/flow-game-mode-v2.css?v=20260925f",
+  "./styles/screens/flow-game-mode-v2.css?v=20260925g",
   "./styles/screens/flow-integration-phase11.css?v=20260916a",
 ]) {
   assert.ok(FLOW_RELEASE_ASSETS.includes(asset), `offline pack missing ${asset}`);
@@ -147,7 +147,7 @@ for (const asset of [
 
 const mainEntry = index.match(/src="js\/main\.js\?v=[^"]+"/)?.[0] || "";
 const mainIndex = mainEntry ? index.indexOf(mainEntry) : -1;
-const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260925i"');
+const releaseIndex = index.indexOf('src="js/flow/flowRuntimeLoader.js?v=20260925j"');
 assert.ok(mainIndex >= 0 && releaseIndex > mainIndex, "main.js must boot before the release loader can temporarily emulate the developer route");
 assert.doesNotMatch(index, /src="js\/flow\/flowPhase1\.js/);
 assert.doesNotMatch(index, /const flowParams = new URLSearchParams/);
