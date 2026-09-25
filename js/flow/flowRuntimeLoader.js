d0e983ae1943ed992ab5ce37007a1f0317963bef
thiepn <junsosmart@gmail.com>
2026-09-25 18:01:41 +0200

    Flow: publish word-boundary accounting v39

--- js/flow/flowRuntimeLoader.js
+++ js/flow/flowRuntimeLoader.js
@@ -4,8 +4,8 @@ import {
 } from "./flowIdentityV1.js?v=20260924b";
 
 const RELEASE_FLAG = "flowRelease";
-const FLOW_RELEASE_VERSION = 38;
-const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v41";
+const FLOW_RELEASE_VERSION = 39;
+const FLOW_RELEASE_CACHE_NAME = "wordstrike-flow-release-v42";
 const FLOW_OFFLINE_CACHE_BATCH_SIZE = 16;
 const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
   "mode",
@@ -30,7 +30,7 @@ const FLOW_RELEASE_QUERY_KEYS = Object.freeze([
 ]);
 
 const FLOW_RELEASE_ASSETS = Object.freeze([
-  "./js/flow/flowRuntimeLoader.js?v=20260925p",
+  "./js/flow/flowRuntimeLoader.js?v=20260925q",
   "./js/leaderboardReturnState.js",
   "./js/pendingResultSubmission.js",
   "./js/submissionOutbox.js",
@@ -62,7 +62,7 @@ const FLOW_RELEASE_ASSETS = Object.freeze([
   "./js/flow/flowContentExpansion.js",
   "./js/flow/flowCorpusV2.js?v=20260923a",
   "./js/flow/flowCorpusHistory.js?v=20260923a",
-  "./js/flow/flowEngine.js?v=20260925b",
+  "./js/flow/flowEngine.js?v=20260925c",
   "./js/flow/flowGameplay.js",
   "./js/flow/flowGameModeV2.js?v=20260923c",
   "./js/flow/flowIntegrationBootstrap.js?v=20260923b",
@@ -73,7 +73,7 @@ const FLOW_RELEASE_ASSETS = Object.freeze([
   "./js/flow/flowModifiers.js",
   "./js/flow/flowModifiersPhase9.js?v=20260923a",
   "./js/flow/flowPassages.js",
-  "./js/flow/flowPhase1.js?v=20260925m",
+  "./js/flow/flowPhase1.js?v=20260925n",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -414,7 +414,7 @@ async function importFlowRuntime() {
     const integrationBootstrap = await import("./flowIntegrationBootstrap.js?v=20260923b");
     integrationBootstrap.applyFlowIntegrationDefaults?.();
     await Promise.all([
-      import("./flowPhase1.js?v=20260925m"),
+      import("./flowPhase1.js?v=20260925n"),
       import("./flowVisualPhase6.js?v=20260923a"),
     ]);
 
