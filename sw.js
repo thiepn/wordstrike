e4bfc81dc05ef7a68ed74df57fe64d84f9be525a
thiepn <junsosmart@gmail.com>
2026-09-25 16:44:18 +0200

    Flow: precache typing surface v30

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "wordstrike-pwa-";
-const CACHE_NAME = CACHE_PREFIX + "v96-flow-completion-integrity";
+const CACHE_NAME = CACHE_PREFIX + "v97-flow-typing-surface-v30";
 const APP_SHELL = [
   "./js/practiceLab/practiceAssessmentInput.js",
   "./js/practiceLab/practiceDurableManifest.js",
@@ -57,7 +57,7 @@ const APP_SHELL = [
   "./js/flow/flowModifiersPhase9.js",
   "./js/flow/flowPassages.js",
   "./js/flow/flowPhase1.js",
-  "./js/flow/flowPhase1.js?v=20260925g",
+  "./js/flow/flowPhase1.js?v=20260925h",
   "./js/flow/flowProgression.js",
   "./js/flow/flowProgression.js?v=20260923a",
   "./js/flow/flowProgressionV4.js",
@@ -69,7 +69,7 @@ const APP_SHELL = [
   "./js/flow/flowRecordsV2.js",
   "./js/flow/flowRecordsV2.js?v=20260923a",
   "./js/flow/flowRuntimeLoader.js",
-  "./js/flow/flowRuntimeLoader.js?v=20260925g",
+  "./js/flow/flowRuntimeLoader.js?v=20260925h",
   "./js/flow/flowSelection.js",
   "./js/flow/flowShell.js",
   "./js/flow/flowState.js",
@@ -78,7 +78,7 @@ const APP_SHELL = [
   "./js/flow/flowUiPhase7KeyboardGuard.js?v=20260923a",
   "./js/flow/flowUiPhase7Polish.js",
   "./js/flow/flowUxPhase8.js",
-  "./js/flow/flowUxPhase8.js?v=20260925c",
+  "./js/flow/flowUxPhase8.js?v=20260925d",
   "./js/flow/flowVisualPhase6.js",
   "./js/nativeBackNavigation.js",
   "./js/speedTestPerformanceV1.js?v=20260924a",
@@ -102,7 +102,7 @@ const APP_SHELL = [
   "./styles/screens/flow-phase5.css",
   "./styles/screens/flow-phase5.css?v=20260916a",
   "./styles/screens/flow-game-mode-v2.css",
-  "./styles/screens/flow-game-mode-v2.css?v=20260925d",
+  "./styles/screens/flow-game-mode-v2.css?v=20260925e",
   "./styles/screens/flow-session-v4.css",
   "./styles/screens/flow-session-v4.css?v=20260924c",
   "./styles/screens/flow-ui-phase7-polish.css",
